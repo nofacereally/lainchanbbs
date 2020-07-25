46eaa48931775a16903349324eb107330f5edabb
No Face <nofacereally@protonmail.com>
2020-07-25 11:10:06 -0500

    adding banner from ops post on >>29010

--- config.py
+++ config.py
@@ -22,27 +22,36 @@ import logging
 
 class config:
 
-    welcome_banner = "https://lainchan.org/static/lain_banner1.png"
+    welcome_banner = None
 
     # Stock Messages
-    welcome_message = '''
-                                                                      
-  _               _____ _   _  _____ _    _          _   _ ____  ____   _____ 
- | |        /\   |_   _| \ | |/ ____| |  | |   /\   | \ | |  _ \|  _ \ / ____|
- | |       /  \    | | |  \| | |    | |__| |  /  \  |  \| | |_) | |_) | (___  
- | |      / /\ \   | | | . ` | |    |  __  | / /\ \ | . ` |  _ <|  _ < \___ \ 
- | |____ / ____ \ _| |_| |\  | |____| |  | |/ ____ \| |\  | |_) | |_) |____) |
- |______/_/    \_\_____|_| \_|\_____|_|  |_/_/    \_\_| \_|____/|____/|_____/ 
-                                                                      
+    welcome_message = '''\x1b[32;1m
+d888888888888888PY88P         Y88Y' `YP"    Y"    "YP"88888888888888PY8
+888888888888888P  Y8           `"`        _.ooo888oo. 8P88888888888'  "
+888888888888"" _.oo88888oo.             `" ,--- __      `8888888888
+88888888888P  "     __ ---.                 oP"88887o.   :Y88888P'8.
+Y8888888888b     .od888"o.`                    Y88P  Y   :. '"Y'_88b
+ ""88888888K    Y. Y88P                      -._____;    888Y _`"Y88.
+   "88888l  l    `.___..-'                               88',d88:d88'
+    Y8P"Y8.  l                                           : 88888:888p
+     "   Y8b.                                            :d88888:8888.
+          `8888b                                        ,8888888:88888b
+           Y8888b                                      d88888888:888888"
+           dbd888b.                ` '                d888888888:888P'
+          d888888888o_             .--             _p88888888888;P'
+         `YP"Y888888888b.                          Y8888P"Y8888
+                ''"YP"""YP=p._             \x1b[31;1m 8""""8   8""""8   8""""8\x1b[32;1m
+e     eeeee e  eeeee eeee e   e eeeee eeeee\x1b[31;1m 8    8   8    8   8     \x1b[32;1m
+8     8   8 8  8   8 8  8 8   8 8   8 8   8\x1b[31;1m 8eeee8ee 8eeee8ee 8eeeee\x1b[32;1m
+8e    8eee8 8e 8e  8 8e   8eee8 8eee8 8e  8\x1b[31;1m 88     8 88     8     88\x1b[32;1m
+88    88  8 88 88  8 88   88  8 88  8 88  8\x1b[31;1m 88     8 88     8 e   88\x1b[32;1m
+88eee 88  8 88 88  8 88e8 88  8 88  8 88  8\x1b[31;1m 88eeeee8 88eeeee8 8eee88\x1b[32;1m
+                             "Partially works!"
+\x1b[0m
 '''
 
     welcome_help_text = '''
-New to the server? Here's a quick guide:
-       lb                      - List all the available boards
-       lt <boardID>            - List the threads for a board
-       ei/di                   - Enable/Disable the showing of images
-
-If you need additional help or examples, try help <command>.
+For help, type help. For additional help, try help <command>.
 '''
     prompt = "lainchanbbs> "
     continue_prompt = "Press any key to continue..."
